0494a4b94c1ebbb50f9d33f0e68f23dab6287993
prixus <simonpetercalamno@gmail.com>
2022-06-04 19:54:02 +0800

    Removed unused codes

--- src/routes/api.php
+++ src/routes/api.php
@@ -15,7 +15,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-//Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-//    return $request->user();
-//});
 Route::get('/definition/{word}', [WordDefinitionController::class, 'fetchWordDefinition']);
