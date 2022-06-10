9b63edfc2bd53aa88e68ac131a66e37c57255dc6
prixus <simonpetercalamno@gmail.com>
2022-06-10 21:29:30 +0800

    Integrated vue router settings in the backend and added npm in the container

--- Dockerfile
+++ Dockerfile
@@ -5,4 +5,7 @@ WORKDIR /var/www/app
 # Get latest Composer
 COPY --from=composer:latest /usr/bin/composer /usr/bin/composer
 
+# Install node
+RUN apk add --update npm
+
 RUN docker-php-ext-install pdo pdo_mysql
--- src/routes/web.php
+++ src/routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
+use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
+use Inertia\Inertia;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +15,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/{any}', function () {
+    return view('main');
+})->where('any', '.*');
